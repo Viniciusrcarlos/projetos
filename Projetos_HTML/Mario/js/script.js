80f98d3f2a05e2e3d78d05f3a2aa73a25900b9b3
Vinicius Raupp <viniciusrauppcarlos@gmail.com>
2022-07-18 21:51:06 -0300

    A

--- Projetos_HTML/Mario/js/script.js
+++ Projetos_HTML/Mario/js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -9,4 +10,26 @@ const jump = () => {
 
 }
 
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '/Projetos_HTML/Mario/img/game-over.png';
+        mario.style.width = '70px'
+
+    }
+
+}, 10);
+
 document.addEventListener('keydown', jump);
+
